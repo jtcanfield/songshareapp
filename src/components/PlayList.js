e91b06f6c7cbef997b69663dee8f797d889bb85f
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-09-15 13:29:55 -0400

    add playlist mapping

--- src/components/PlayList.js
+++ src/components/PlayList.js
@@ -2,30 +2,37 @@ import React, { Component } from 'react';
 import PlayListItem from './PlayListItem.js';
 
 export default class PlayList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dataset: []
+    };
+  }
   componentDidMount() {
     fetch('https://tiny-lasagna-server.herokuapp.com/collections/playlisting').then(results => {
       return results.json();
     }).then(data => {
-      console.log(data)
-      const fulldata = res.data;
-      this.setState({ listofLivequakes });
+      this.setState({ dataset: data });
     })
   }
   render() {
-    let playlistitems = earthquakes.features.map((quake) =>{
-      return (
-      <div className="col-sm-6" key={quake.id}>
-        <div className="card" >
-          <div className="card-block">
-
-          </div>
-        </div>
-      </div>
-    )
-    });
+    this.state.dataset.map(x =>{
+      console.log(x);
+    })
     return (
       <div className="PlayList">
-        {playlistitems}
+        {this.state.dataset.map(x =>
+          <div className="col-sm-6" key={x._id}>
+            <div className="card" >
+              <div className="card-block">
+                <p>{x.userName}</p>
+                <p>{x.songTitle}</p>
+                <p>{x.songArtist}</p>
+                <p>{x.songNotes}</p>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
