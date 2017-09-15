8cb82f3d0b157f0b233737831f9b861b95905caf
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-09-15 13:48:56 -0400

    simplify form

--- src/components/PlayListForm.js
+++ src/components/PlayListForm.js
@@ -6,12 +6,10 @@ export default class PlayListForm extends Component {
   this.handleSubmit = this.handleSubmit.bind(this);
   this.handleTextChange = this.handleTextChange.bind(this);
   this.state = {
+    userName: "",
     songTitle: "",
-    songnamevalue: "",
     songArtist: "",
-    artistvalue: "",
     songNotes: "",
-    notesvalue: ""
     };
   }
   componentDidMount(){
@@ -20,33 +18,41 @@ export default class PlayListForm extends Component {
   handleTextChange(event){
     event.preventDefault();
     //using [ ] can turn a string into an object selector
-    this.setState({[event.target.id+"value"]: event.target.value});
+    this.setState({[event.target.id]: event.target.value});
   }
   handleSubmit(event){
     event.preventDefault();
-    this.setState(
-      {
-        songTitle: this.state.songnamevalue,
-        songArtist: this.state.artistvalue,
-        songNotes: this.state.notesvalue
+    this.setState({userName: this.state.userName, songTitle: this.state.songTitle, songArtist: this.state.songArtist, songNotes: this.state.songNotes});
+    let listItem = JSON.stringify(this.state);
+    console.log(listItem);
+    fetch("https://tiny-lasagna-server.herokuapp.com/collections/playlisting", {
+      method: "POST",
+      body: listItem,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
       }
-    )
+    }).then(response => {
+      console.log(response, "yay");
+    }).catch(err => {
+      console.log(err, "boo!");
+    });
+    this.setState({userName: '', songNotes: '', songArtist: '', songTitle:''});
   }
   render() {
     return (
       <div className="PlayListForm">
         <form onSubmit={this.handleSubmit}>
-          <input onChange={this.handleTextChange} type="text" id="artist"
-          placeholder="Artist/Band" value={this.state.artistvalue}/>
-          <input onChange={this.handleTextChange} type="text" id="songname"
-          placeholder="Song Name" value={this.state.songnamevalue}/>
-          <input onChange={this.handleTextChange} type="textarea" id="notes"
-          placeholder="Notes about Song:" value={this.state.notesvalue}/>
+          <input onChange={this.handleTextChange} type="text" id="userName"
+          placeholder="Whats you name?" value={this.state.userName}/>
+          <input onChange={this.handleTextChange} type="text" id="songArtist"
+          placeholder="Artist/Band" value={this.state.songArtist}/>
+          <input onChange={this.handleTextChange} type="text" id="songTitle"
+          placeholder="Song Name" value={this.state.songTitle}/>
+          <input onChange={this.handleTextChange} type="textarea" id="songNotes"
+          placeholder="Notes about Song:" value={this.state.songNotes}/>
           <button type="submit">"SUBMIT"</button>
         </form>
-        <p>{this.state.songTitle}
-        {this.state.songArtist}
-        {this.state.songNotes}</p>
       </div>
     );
   }
